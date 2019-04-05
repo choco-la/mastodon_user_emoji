09b96a857be09afb9ffce1cf017528f4567a6b5c
hyoro-were <choco-la@users.noreply.github.com>
2019-04-05 14:49:29 +0900

    replace display-name

--- ts/insert_user_emoji.ts
+++ ts/insert_user_emoji.ts
@@ -1,5 +1,5 @@
 import { getAccounts, nameToID } from './API'
-import { replaceContent } from './replace_content'
+import { generateContentPattern, replaceContent } from './replace_content'
 
 const ICON_STYLE: { height: string, width: string } = {
   height: '1.2em',
@@ -13,10 +13,11 @@ const getUserIconURL = async (screenName: string): Promise<string> => {
   return accounts.avatar
 }
 
-const replaceUserEmoji = async (innerHTML: string, screenName: string): Promise<string> => {
+const replaceUserEmoji = async (innerHTML: string, screenName: string, regex: RegExp): Promise<string> => {
   const userIconSrc = await getUserIconURL(screenName)
   const iconHTMLStrings = `<img src="${userIconSrc}" style='height: ${ICON_STYLE.height}; width: ${ICON_STYLE.width};'>`
-  return replaceContent(innerHTML, iconHTMLStrings, screenName)
+
+  return replaceContent(innerHTML, iconHTMLStrings, regex)
 }
 
 const replaceContentParagraph = async (content: HTMLParagraphElement) => {
@@ -25,7 +26,20 @@ const replaceContentParagraph = async (content: HTMLParagraphElement) => {
   if (!matches) return html
   for (const match of matches) {
     const screenName = match.slice(2, -1)
-    html = await replaceUserEmoji(html, screenName)
+    const pattern = generateContentPattern(screenName)
+    const re = new RegExp(pattern, 'iu')
+    html = await replaceUserEmoji(html, screenName, re)
+  }
+  return html
+}
+
+const replaceDisplayName = async (content: HTMLSpanElement) => {
+  const matches = content.innerText.match(userIconRegex)
+  let html = content.innerHTML
+  if (!matches) return html
+  for (const match of matches) {
+    const screenName = match.slice(2, -1)
+    html = await replaceUserEmoji(html, screenName, userIconRegex)
   }
   return html
 }
@@ -34,5 +48,10 @@ export const insertUserEmoji: (element: Element) => void = async (element) => {
   const content = element.querySelector('p')
   if (content) {
     content.innerHTML = await replaceContentParagraph(content)
+  }
+
+  const displayName = element.querySelector('span.display-name') as HTMLSpanElement | null
+  if (displayName) {
+    displayName.innerHTML = await replaceDisplayName(displayName)
   }
 }
--- ts/replace_content.ts
+++ ts/replace_content.ts
@@ -1,7 +1,8 @@
 const PARSER = new DOMParser()
 
-export const replaceContent = (originHTML: string, newHTML: string, screenName: string, _origin?: string): string => {
+export const generateContentPattern = (screenName: string, _origin?: string) => {
   if (!_origin) _origin = window.origin
+
   const space = String.raw`[\s\t]*`
   const prefix = String.raw`:${space}<span class="h-card">${space}<a href="${_origin}/@${screenName}`
   const middle0 = String.raw`" class="u-url mention status-link" title="${screenName}`
@@ -9,8 +10,10 @@ export const replaceContent = (originHTML: string, newHTML: string, screenName: 
   const suffix = String.raw`</span>${space}</a>${space}</span>${space}:`
 
   const pattern = `${prefix}${middle0}${middle1}${suffix}`
-  const re = new RegExp(pattern, 'iu')
+  return pattern
+}
 
+export const replaceContent = (originHTML: string, newHTML: string, regex: RegExp): string => {
   const dom = PARSER.parseFromString(originHTML, 'text/html')
-  return dom.body.innerHTML.replace(re, newHTML)
+  return dom.body.innerHTML.replace(regex, newHTML)
 }
